f290894acd25bf674ea847c09194ff290680722e
ana-kovacevic <ananp.90@gmail.com>
2017-10-10 23:05:54 +0200

    TS Visualizations

--- Hmm_activity.py
+++ Hmm_activity.py
@@ -3,8 +3,10 @@ import pandas as pd
 from matplotlib import cm, pyplot as plt
 from matplotlib.dates import YearLocator, MonthLocator, DayLocator
 import numpy as np
+import seaborn as sns
 
-data = pd.read_csv('D:\\Posao\\Projekti\\City4Age\\activities_out.csv')
+data = pd.read_csv('D:\\Posao\\Projekti\\activities_out.csv')
+
 oneexample = data[data['user_in_role_id'] ==  66] # get one user
 oneexample = oneexample[oneexample['detection_variable_name'] =='walk_distance'] # select one activity
 
@@ -12,9 +14,13 @@ oneexample
 Y = oneexample[['Normalised']]
 X = oneexample[['measure_value']]
 
-type([X])
+Dates=oneexample[['interval_end']]
 
-# Make an HMM instance and execute fit
+type(['interval_end'])
+
+###################################### Make an HMM instance and execute fit
+
+#################### Train on Normalised value (Y)
 model = GaussianHMM(n_components=3, covariance_type="diag", n_iter=1000).fit(Y)
 
 # Predict the optimal sequence of internal hidden state
@@ -22,12 +28,9 @@ hidden_states = model.predict(Y)
 
 print("done")
 
-
-
 print("Transition matrix")
 print(model.transmat_)
 print()
-
 
 #### Print model parameters
 
@@ -44,7 +47,79 @@ for i in range(model.n_components):
 
 dates=oneexample['interval_end']
 
+#### Subplot of states
+fig, axs = plt.subplots(model.n_components, sharex=True, sharey=True)
+colours = cm.rainbow(np.linspace(0, 1, model.n_components))
+for i, (ax, colour) in enumerate(zip(axs, colours)):
+    # Use fancy indexing to plot data in each state.
+    mask = hidden_states == i
+    ax.plot_date(dates[mask], Y[mask], ".-", c=colour)
+    ax.set_title("{0}th hidden state".format(i))
 
+    # Format the ticks.
+    #ax.xaxis.set_major_locator(YearLocator())
+    ax.xaxis.set_minor_locator(MonthLocator())
+    ax.xaxis.set_minor_locator(DayLocator())
+
+    ax.grid(True)
+
+plt.show()
+############################### Color time series by States
+tsY = pd.DataFrame(Y['Normalised'])
+tsY['Date'] = dates
+selY = tsY.ix[:].dropna()
+
+selY['Date'] = pd.to_datetime(selY['Date'])
+selY.set_index('Date', inplace=True)
+stateY = (pd.DataFrame(hidden_states, columns=['state'], index=selY.index)
+          .join(selY, how='inner')
+          .assign(vol_cret=selY.Normalised.cumsum())
+          .reset_index(drop=False)
+          .rename(columns={'index':'Date'}))
+print(stateY.head())
+
+a = stateY['state']
+x = stateY.index
+y = stateY['Normalised']
+for a, x1, x2, y1, y2 in zip(a[1:], x[:-1], x[1:], y[:-1], y[1:]):
+    if a == 0:
+        plt.plot([x1, x2], [y1, y2], 'r')
+    elif a == 1:
+        plt.plot([x1, x2], [y1, y2], 'g')
+    else:
+        plt.plot([x1, x2], [y1, y2], 'b')
+
+plt.show()
+
+
+#################### Train on  value (X)
+model = GaussianHMM(n_components=3, covariance_type="diag", n_iter=1000).fit(X)
+
+# Predict the optimal sequence of internal hidden state
+hidden_states = model.predict(X)
+
+print("done")
+
+print("Transition matrix")
+print(model.transmat_)
+print()
+
+#### Print model parameters
+
+print("Transition matrix")
+print(model.transmat_)
+print()
+
+print("Means and vars of each hidden state")
+for i in range(model.n_components):
+    print("{0}th hidden state".format(i))
+    print("mean = ", model.means_[i])
+    print("var = ", np.diag(model.covars_[i]))
+    print()
+
+dates=oneexample['interval_end']
+
+#### Subplot of states
 fig, axs = plt.subplots(model.n_components, sharex=True, sharey=True)
 colours = cm.rainbow(np.linspace(0, 1, model.n_components))
 for i, (ax, colour) in enumerate(zip(axs, colours)):
@@ -54,10 +129,36 @@ for i, (ax, colour) in enumerate(zip(axs, colours)):
     ax.set_title("{0}th hidden state".format(i))
 
     # Format the ticks.
-    ax.xaxis.set_major_locator(YearLocator())
+    #ax.xaxis.set_major_locator(YearLocator())
     ax.xaxis.set_minor_locator(MonthLocator())
     ax.xaxis.set_minor_locator(DayLocator())
 
     ax.grid(True)
+
+plt.show()
+############################### Color time series by States
+tsX = pd.DataFrame(Y['Normalised'])
+tsX['Date'] = dates
+selX = tsX.ix[:].dropna()
+
+selX['Date'] = pd.to_datetime(selX['Date'])
+selX.set_index('Date', inplace=True)
+stateX = (pd.DataFrame(hidden_states, columns=['state'], index=selX.index)
+          .join(selX, how='inner')
+          .assign(vol_cret=selX.measure_value.cumsum())
+          .reset_index(drop=False)
+          .rename(columns={'index':'Date'}))
+print(stateX.head())
+
+a = stateX['state']
+x = stateX.index
+y = stateX['measure_value']
+for a, x1, x2, y1, y2 in zip(a[1:], x[:-1], x[1:], y[:-1], y[1:]):
+    if a == 0:
+        plt.plot([x1, x2], [y1, y2], 'r')
+    elif a == 1:
+        plt.plot([x1, x2], [y1, y2], 'g')
+    else:
+        plt.plot([x1, x2], [y1, y2], 'b')
 
 plt.show()
--- readdata.py
+++ readdata.py
@@ -5,4 +5,5 @@ Created on Sat Oct  7 21:36:54 2017
 @author: akovacevic
 """
 import pandas as pd
-data = data = pd.read_csv('D:\\Posao\\Projekti\\City4Age\\activities_out.csv')
+data = pd.read_csv('D:\\Posao\\Projekti\\City4Age\\activities_out.csv')
+data = pd.read_csv('C:\\Users\\Ana Kovacevic\\Documents\\DataForActivityAnalysis\\activities_out.csv')
