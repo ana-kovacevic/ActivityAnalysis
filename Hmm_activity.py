c5651ea22c27244db392e571dbe231584cb8bdb5
vucko83 <vucko83@gmail.com>
2017-10-30 21:49:15 +0100

    Just checking...

--- Hmm_activity.py
+++ Hmm_activity.py
@@ -1,11 +1,193 @@
+
 from hmmlearn.hmm import GaussianHMM
 import pandas as pd
 from matplotlib import cm, pyplot as plt
 from matplotlib.dates import YearLocator, MonthLocator, DayLocator
 import numpy as np
-import seaborn as sns
+import datetime as dt
 
-data = pd.read_csv('D:\\Posao\\Projekti\\activities_out.csv')
+data = pd.read_csv('activities_out.csv')
+
+
+users=data['user_in_role_id'].unique()
+activities=data['detection_variable_name'].unique()
+
+
+pd.unique(data[['user_in_role_id', 'interval_end']].values)
+
+
+#data.shape[0]
+
+#user_data=data[data['user_in_role_id']==66]
+#activities=user_data['detection_variable_name'].unique()
+#dates=user_data[user_data['detection_variable_name']=='walk_steps']
+
+
+##### Select users and activities
+
+users=[66]
+
+data[data['user_in_role_id'].isin(users)]
+
+activities_for_multi=['sleep_awake_time','sleep_deep_time', 'sleep_light_time', 'sleep_tosleep_time']
+
+
+
+#get_users_activities(data, 66)
+
+user=66
+
+
+
+
+
+def get_users_activities(data, user):
+    user_data=data[data['user_in_role_id']==user]
+    d = user_data.groupby(['user_in_role_id', 'detection_variable_name'])['measure_value'].count()
+    d.rename(columns={'measure_value':'count_measure_value'}, inplace=True)
+    d=pd.DataFrame(d)
+    return d
+
+#### Check all activities and counts of appearences for single user
+def select_pivot_users_activities(data, user, activities):
+    user_data=data[data['user_in_role_id']==user]
+    user_data=user_data[user_data['detection_variable_name'].isin(activities)]
+    pivot_data = user_data.pivot_table(index=['user_in_role_id', 'interval_end'], columns='detection_variable_name',values='measure_value')
+    return pivot_data
+
+
+#### Create single user single multiple activities cluster
+
+activities=['sleep_awake_time','sleep_deep_time', 'sleep_light_time', 'sleep_tosleep_time']
+user=66
+pivoted_data=select_pivot_users_activities(data, user, activities)
+pivoted_data.head()
+pivoted_data[activities]
+model = GaussianHMM(n_components=3, covariance_type="full", n_iter=1000).fit(pivoted_data[activities])
+hidden_states=model.predict(pivoted_data)
+
+
+Y=pivoted_data['sleep_awake_time']
+Y=Y.reset_index()
+Y=Y[['sleep_awake_time']]
+Z=pivoted_data['sleep_deep_time']
+Z=Z.reset_index()
+Z=Z[['sleep_deep_time']]
+
+dates=pivoted_data.index.get_level_values(1)
+dates=pd.Series(dates)
+
+dates=pd.to_datetime(dates)
+#format='%d/%m/%Y')
+
+
+
+
+### Subplot the states multi-variate single user
+fig, axs = plt.subplots(model.n_components, sharex=True, sharey=True)
+colours = cm.rainbow(np.linspace(0, 1, model.n_components))
+i=0
+for ax in axs:
+    # Use fancy indexing to plot data in each state.
+    mask = hidden_states == i
+    i=i+1
+    ax.plot_date(dates[mask], Y[mask], ".-", c=colours[0])
+    ax.plot_date(dates[mask], Z[mask], ".-", c=colours[1])
+    ax.set_title("{0}th hidden state".format(i))
+    # Format the ticks.
+    #ax.xaxis.set_major_locator(YearLocator())
+    ax.xaxis.set_minor_locator(MonthLocator())
+    ax.xaxis.set_minor_locator(DayLocator())
+    #ax.grid(True)
+    #plt.suptitle("User_in_role_id: " + str(results[0]) + "     Activity: "+str(results[1]))
+    #plt.savefig(path_store + 'user_' + str(results[0])+ '_activity_'+str(results[1])+'.png', bbox_inches='tight')
+plt.show()
+
+
+
+
+#### Create single user single activity cluster
+results_single_hmm =[]
+for user in users:
+    user_data=data[data['user_in_role_id']==user]
+    activities=user_data['detection_variable_name'].unique()
+    for ac in activities:
+        user_ac=user_data[user_data['detection_variable_name']==ac]
+        if user_ac.shape[0]>30:
+            normalized=user_ac[['Normalised']]
+            original=user_ac[['measure_value']]
+            dates=user_ac[['interval_end']]
+            model = GaussianHMM(n_components=5, covariance_type="diag", n_iter=1000).fit(normalized)
+            predictions=model.predict(normalized)
+            results_single_hmm.append((user, ac, model, predictions, dates, normalized, original))
+
+
+
+
+### Find optimal number of clusters
+
+activities=results_single_hmm[4][5]
+ks=range(2,11)
+
+#find_optimal_HMM(activities,ks)
+#draw_clusters(results_single_hmm[4])
+
+
+# selektuj uset/activity
+def find_optimal_HMM(activities, cluster_range):
+    scores=[]
+    for k in cluster_range:
+        model=GaussianHMM(n_components=k, covariance_type="diag", n_iter=1000).fit(activities)
+        log_likelihood=model.score(activities)
+        bic=2*log_likelihood-k*np.log(len(activities))
+        scores.append((k, log_likelihood, bic))
+    return scores
+
+
+
+
+
+
+model.score()
+
+
+### Print or save clusters
+#for result in results_single_hmm:
+#    draw_clusters(result)
+
+### Ploting complete behavior
+
+
+#### Subplot of states sigle variate single user
+
+def draw_clusters(results, path_store='Single_Activity_Clusters/De-normalized/'):
+    model=results[2]
+    hidden_states=results[3]
+    dates=results[4]
+    Y=results[6]
+    fig, axs = plt.subplots(model.n_components, sharex=True, sharey=True)
+    colours = cm.rainbow(np.linspace(0, 1, model.n_components))
+    for i, (ax, colour) in enumerate(zip(axs, colours)):
+        # Use fancy indexing to plot data in each state.
+        mask = hidden_states == i
+        ax.plot_date(dates[mask], Y[mask], ".-", c=colour)
+        ax.set_title("{0}th hidden state".format(i+1))
+        # Format the ticks.
+        ax.xaxis.set_major_locator(YearLocator())
+        ax.xaxis.set_minor_locator(MonthLocator())
+        ax.xaxis.set_minor_locator(DayLocator())
+        ax.grid(True)
+    plt.suptitle("User_in_role_id: " + str(results[0]) + "     Activity: "+str(results[1]))
+    #plt.savefig(path_store + 'user_' + str(results[0])+ '_activity_'+str(results[1])+'.png', bbox_inches='tight')
+    #plt.show()
+
+
+'''
+
+
+
+
+
 
 oneexample = data[data['user_in_role_id'] ==  68] # get one user
 oneexample = oneexample[oneexample['detection_variable_name'] =='walk_distance'] # select one activity
@@ -161,3 +343,4 @@ for a, x1, x2, y1, y2 in zip(a, x, x[1:], y, y[1:]):
     else:
         plt.plot([x1, x2], [y1, y2], 'purple')
 plt.show()
+'''
