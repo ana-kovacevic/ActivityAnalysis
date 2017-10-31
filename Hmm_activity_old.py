b201c9e709f5649092318287002ade4e4e0d7ddd
vucko83 <vucko83@gmail.com>
2017-10-31 12:57:32 +0100

    Just checking...

--- Hmm_activity_old.py
+++ Hmm_activity_old.py
@@ -54,26 +54,27 @@ dates=(pivoted_data['interval_end']).dt.to_pydatetime()
 
 
 
-#### Print model parameters
-#maybe add whaole covariances
-def print_hmm_params(model):
-    print("Transition matrix")
-    print(model.transmat_)
-    print()
-
-    print("Means and vars of each hidden state")
-    for i in range(model.n_components):
-        print("{0}th hidden state".format(i))
-        print("mean = ", model.means_[i])
-        print("var = ", np.diag(model.covars_[i]))
-        print()
 
 
 
 
-print_hmm_params(model)
 
-##### Plot model results (means and variances)
+
+'''
+tsY = pd.DataFrame(Y['Normalised'])
+tsY['Date'] = dates
+selY = tsY.ix[:].dropna()
+
+selY['Date'] = pd.to_datetime(selY['Date'])
+selY.set_index('Date', inplace=True)
+stateY = (pd.DataFrame(hidden_states, columns=['state'], index=selY.index)
+          .join(selY, how='inner')
+          .assign(vol_cret=selY.Normalised.cumsum())
+          .reset_index(drop=False)
+          .rename(columns={'index':'Date'}))
+print(stateY.head())
+'''
+
 
 
 ### Subplot the states multi-variate single user - By States
@@ -303,8 +304,96 @@ for a, x1, x2, y1, y2 in zip(a[1:], x[:-1], x[1:], y[:-1], y[1:]):
 plt.show()
 
 
+#################### Train on  value (X)
+model = GaussianHMM(n_components=3, covariance_type="diag", n_iter=1000).fit(X)
 
+# Predict the optimal sequence of internal hidden state
+hidden_states = model.predict(X)
 
+print("done")
 
+print("Transition matrix")
+print(model.transmat_)
+print()
 
+#### Print model parameters
+
+print("Transition matrix")
+print(model.transmat_)
+print()
+
+print("Means and vars of each hidden state")
+for i in range(model.n_components):
+    print("{0}th hidden state".format(i))
+    print("mean = ", model.means_[i])
+    print("var = ", np.diag(model.covars_[i]))
+    print()
+
+dates=oneexample['interval_end']
+
+#### Subplot of states
+fig, axs = plt.subplots(model.n_components, sharex=True, sharey=True)
+colours = cm.rainbow(np.linspace(0, 1, model.n_components))
+for i, (ax, colour) in enumerate(zip(axs, colours)):
+    # Use fancy indexing to plot data in each state.
+    mask = hidden_states == i
+    ax.plot_date(dates[mask], X[mask], ".-", c=colour)
+    ax.set_title("{0}th hidden state".format(i))
+
+    # Format the ticks.
+    #ax.xaxis.set_major_locator(YearLocator())
+    ax.xaxis.set_minor_locator(MonthLocator())
+    ax.xaxis.set_minor_locator(DayLocator())
+
+    ax.grid(True)
+
+plt.show()
+############################### Color time series by States
+tsX = pd.DataFrame(X['measure_value'])
+tsX['Date'] = dates
+selX = tsX.ix[:].dropna()
+
+selX['Date'] = pd.to_datetime(selX['Date'])
+selX.set_index('Date', inplace=True)
+stateX = (pd.DataFrame(hidden_states, columns=['state'], index=selX.index)
+          .join(selX, how='inner')
+          .assign(vol_cret=selX.measure_value.cumsum())
+          .reset_index(drop=False)
+          .rename(columns={'index':'Date'}))
+print(stateX.head())
+
+a = stateX['state']
+x = stateX.index
+y = stateX['measure_value']
+for a, x1, x2, y1, y2 in zip(a, x, x[1:], y, y[1:]):
+    if a == 0:
+        plt.plot([x1, x2], [y1, y2], 'green')
+    elif a == 1:
+        plt.plot([x1, x2], [y1, y2], 'red')
+    else:
+        plt.plot([x1, x2], [y1, y2], 'purple')
+plt.show()
 '''
+
+#users=data['user_in_role_id'].unique()
+#activities=data['detection_variable_name'].unique()
+#pd.unique(data[['user_in_role_id', 'interval_end']].values)
+#data.shape[0]
+#user_data=data[data['user_in_role_id']==66]
+#activities=user_data['detection_variable_name'].unique()
+#dates=user_data[user_data['detection_variable_name']=='walk_steps']
+
+
+##### Select users and activities
+
+#users=[66]
+
+#data[data['user_in_role_id'].isin(users)]
+
+#activities_for_multi=['sleep_awake_time','sleep_deep_time', 'sleep_light_time', 'sleep_tosleep_time']
+
+
+
+#get_users_activities(data, 66)
+
+#user=66
