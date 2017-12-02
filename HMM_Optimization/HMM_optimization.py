b5fe790f265af1eaf07640643d9778b6193cd053
vucko83 <vucko83@gmail.com>
2017-12-02 22:23:45 +0100

    Add HMM optimization

---
+++ HMM_Optimization/HMM_optimization.py
@@ -0,0 +1,67 @@
+from hmmlearn.hmm import GaussianHMM
+import data_preparation as dp
+import numpy as np
+
+import pandas as pd
+
+
+### Creates single variate HMM models for each activity.
+### This method should only be called. It calls preparation methods and returns dictionary with elements for plotting and mapping to states
+def create_single_variate_clusters(data, user, activities, activity_extremization, activity_weights):
+    clusters_activities = {}
+    for ac in activities:
+        pivoted_data=dp.prepare_data(data, user, ac)
+        model = GaussianHMM(n_components=5, covariance_type="full", n_iter=1000).fit(pivoted_data.iloc[:, 2:])
+        hidden_states = model.predict(pivoted_data.iloc[:, 2:])
+        extreme=activity_extremization[ac]
+        weight=activity_weights[ac]
+        clusters_activities.update({ac:{'name': ac, 'model':model, 'clusters':hidden_states, 'values':pivoted_data[ac], 'dates':pivoted_data['interval_end'], 'extremization':extreme, 'weight':weight}})
+    return clusters_activities
+
+
+
+
+def optimize_number_of_clusters(data, range_of_clusters):
+    '''    
+    :param data: 
+    :param range_of_clusters: 
+    :return: 
+    '''
+
+    best_value=np.inf # create for maximization and minimization
+    best_model=None
+    for n_states in range_of_clusters:
+        model = GaussianHMM(n_components=n_states, covariance_type="full", n_iter=1000).fit(data)
+        log_likelihood = model.score(data)
+        criteria=bic_criteria(data, log_likelihood, model)
+        if criteria < best_value:
+            best_value, best_model = criteria, model
+    return best_value, best_model
+
+
+def bic_criteria(data, log_likelihood, model):
+    '''
+    :param data: 
+    :param log_likelihood: 
+    :param model: 
+    :return: 
+    '''
+    n_features = data.shape(2)  ### here adapt for multi-variate
+    n_states=len(model.means_)
+    n_params = n_states * (n_states - 1) + 2 * n_features * n_states
+    logN = np.log(len(data))
+    bic = -2 * log_likelihood + n_params * logN
+    return(bic)
+
+def aic_criteria(data, log_likelihood, model):
+    '''
+    :param data: 
+    :param log_likelihood: 
+    :param model: 
+    :return: 
+    '''
+    n_features = data.shape(2)  ### here adapt for multi-variate
+    n_states=len(model.means_)
+    n_params = n_states * (n_states - 1) + 2 * n_features * n_states
+    aic = -2 * log_likelihood + n_params
+    return(aic)
--- univariate_hmms.py
+++ univariate_hmms.py
@@ -1,48 +1,9 @@
-from hmmlearn.hmm import GaussianHMM
 import pandas as pd
+import data_preparation as dp
+import HMM_Optimization as hmm_opt
 from matplotlib import cm, pyplot as plt
 from matplotlib.dates import YearLocator, MonthLocator, DayLocator
 import numpy as np
-
-
-
-
-### Creates single variate HMM models for each activity.
-### This method should only be called. It calls preparation methods and returns dictionary with elements for plotting and mapping to states
-def create_single_variate_clusters(data, activities, activity_extremization, activity_weights):
-    clusters_activities = {}
-    for ac in activities:
-        pivoted_data=prepare_data(data, user, ac)
-        model = GaussianHMM(n_components=5, covariance_type="full", n_iter=1000).fit(pivoted_data.iloc[:, 2:])
-        hidden_states = model.predict(pivoted_data.iloc[:, 2:])
-        extreme=activity_extremization[ac]
-        weight=activity_weights[ac]
-
-        clusters_activities.update({ac:{'name': ac, 'model':model, 'clusters':hidden_states, 'values':pivoted_data[ac], 'dates':pivoted_data['interval_end'], 'extremization':extreme, 'weight':weight}})
-    return clusters_activities
-
-
-
-
-def optimize_number_of_clusters(data, range_of_clusters):
-    '''    
-    :param data: 
-    :param range_of_clusters: 
-    :return: 
-    '''
-
-    best_value=np.inf # create for maximization and minimization
-    best_model=None
-    for n_states in range_of_clusters:
-        model = GaussianHMM(n_components=n_states, covariance_type="diag", n_iter=1000).fit(data)
-        log_likelihood = model.score(data)
-        n_features = 1 ### here adapt for multi-variate
-        n_params = n_states * (n_states - 1) + 2 * n_features * n_states
-        logN = np.log(len(data))
-        bic = -2 * log_likelihood + n_params * logN
-        if bic < best_value:
-            best_value, best_model = bic, model
-    return best_value, best_model
 
 
 '''
@@ -51,16 +12,18 @@ PROGRAM LOGIC
 ###########################################################################
 '''
 
-import data_preparation
-import pandas as pd
-data = pd.read_csv('activities_out.csv') # Reads data with original and normalized values for each user and activity
-
-from data_preparation import get_users_activities
-
-import data_preparation as dp
+data = pd.read_csv('Data/activities_out.csv') # Reads data with original and normalized values for each user and activity
+dp.get_users_activities(data, 66)
 
 
-dp.get_users_activities(data, 14)
+user=66 # selects one user id
+activities=['sleep_awake_time','sleep_deep_time', 'sleep_light_time', 'sleep_tosleep_time', 'sleep_wakeup_num']
+activity_extremization = {'sleep_light_time':'max', 'sleep_deep_time':'max', 'sleep_awake_time':'min', 'sleep_wakeup_num':'min', 'sleep_tosleep_time':'min'}
+activity_weights = {'sleep_light_time':0.1, 'sleep_deep_time':0.3, 'sleep_awake_time':0.1, 'sleep_wakeup_num':0.3, 'sleep_tosleep_time':0.2}
+
+hmm_opt.
+
+
 
 
 
@@ -78,10 +41,6 @@ data.head(2)
 
 data.head(5)
 
-user=66 # selects one user id
-activities=['sleep_awake_time','sleep_deep_time', 'sleep_light_time', 'sleep_tosleep_time', 'sleep_wakeup_num']
-activity_extremization = {'sleep_light_time':'max', 'sleep_deep_time':'max', 'sleep_awake_time':'min', 'sleep_wakeup_num':'min', 'sleep_tosleep_time':'min'}
-activity_weights = {'sleep_light_time':0.1, 'sleep_deep_time':0.3, 'sleep_awake_time':0.1, 'sleep_wakeup_num':0.3, 'sleep_tosleep_time':0.2}
 
 pivoted_data=prepare_data(data, user, 'sleep_tosleep_time')
 
