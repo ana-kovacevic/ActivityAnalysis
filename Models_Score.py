b3e72d93c7ae1d446c382ded8767e6a1ad11b93a
vucko83 <vucko83@gmail.com>
2017-12-10 01:30:47 +0100

    Automating ploting single variate

--- Models_Score.py
+++ Models_Score.py
@@ -5,6 +5,11 @@ import data_preparation as dp
 import numpy as np
 
 
+'''
+###############################
+SINGLE VARIATE
+##############################
+'''
 
 def get_users_activities(users_activities_models):
     '''
@@ -22,6 +27,10 @@ def get_users_activities(users_activities_models):
 users_activities = get_users_activities(users_activities_models)
 
 def predict_single_variate(users_activities):
+    '''
+    :param users_activities: 
+    :return: 
+    '''
     df_predictions=pd.DataFrame()
     for user, activities in users_activities.items():
         for activity in activities:
@@ -40,14 +49,6 @@ def predict_single_variate(users_activities):
 
 data=pd.read_csv('Data/activities_out.csv')
 
-### Maybe add preparation part
-users=[66, 67, 68]
-
-'''
-###############################
-SINGLE VARIATE
-##############################
-'''
 json_users_activities_models=open('Models/HMM/JSON/single_variate_hmms.json').read()
 users_activities_models=json.loads(json_users_activities_models)
 predictions=predict_single_variate(users_activities)
@@ -61,26 +62,13 @@ MULTI VARIATE
 ##########################
 '''
 
-json_users_activities_models=open('Models/HMM/JSON/multi_variate_hmms.json').read()
-users_activities_models=json.loads(json_users_activities_models)
-
-users_ges_activities=get_users_ges_activities(users_activities_models)
-
-
-user=66
-subfactor_activities = dp.get_dict_ges_activities()
-subfactor='quality_of_sleep'
-activities=subfactor_activities[subfactor]
-model=pickle_hmm.load_pickle_hmm_multi_variate(user, subfactor)
-prepared_data = dp.prepare_data(data,user,activities)
-prepared_data.head()
-model.predict(prepared_data.iloc[: ,2:])
-model.predict_proba(prepared_data.iloc[: ,2:])
-
-
 
 
 def get_users_ges_activities(users_ges_activities_models):
+    '''
+    :param users_ges_activities_models: 
+    :return: 
+    '''
     dict={}
     for user, ges_activities_models in users_ges_activities_models.items():
         dict_ges={}
@@ -94,6 +82,11 @@ def get_users_ges_activities(users_ges_activities_models):
 
 
 def predict_multi_variate(data, users_ges_activities):
+    '''
+    :param data: 
+    :param users_ges_activities: 
+    :return: 
+    '''
     for user, ges_activities in users_ges_activities.items():
         for ges, activities in ges_activities.items():
             model=pickle_hmm.load_pickle_hmm_multi_variate(user, ges)
@@ -108,30 +101,11 @@ def predict_multi_variate(data, users_ges_activities):
             prep_data.to_csv('Data/clustered_data/multi_variate_clusters/citizen_id_'+str(user)+'_'+ges+'.csv')
     return 0
 
-
-
-
+json_users_activities_models=open('Models/HMM/JSON/multi_variate_hmms.json').read()
+users_activities_models=json.loads(json_users_activities_models)
+users_ges_activities=get_users_ges_activities(users_activities_models)
 a=predict_multi_variate(data, users_ges_activities)
 
-type(a[1])
-for i in range(len(a)):
-    a[i].shape()
-
-
-a.head()
-
-
-
-
-    for user in users:
-        for subfactor in subfactor_activities.keys():
-                activities=subfactor_activities[subfactor]
-                prepared_data = dp.prepare_data(data,user,activities)
-                best_value, best_model = optimize_number_of_clusters(prepared_data.iloc[: ,2:], list(range(2,11)), cov_type)
-                dict_subfactor.update({subfactor:{'model':best_model, 'activities':activities}})
-        dict_user={user:dict_subfactor}
-        optimal_hmms_multi_variate.update(dict_user)
-    return optimal_hmms_multi_variate
 
 
 
