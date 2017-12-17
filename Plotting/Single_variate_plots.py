5dc1c5c1bdc0932a9b488476058ee4561decb803
ana-kovacevic <ananp.90@gmail.com>
2017-12-17 01:27:23 +0100

    new plot and small changes

--- Plotting/Single_variate_plots.py
+++ Plotting/Single_variate_plots.py
@@ -31,7 +31,7 @@ def create_single_variate_plot(data, user, activity):
         ax.grid(True)
         # plt.suptitle("User_in_role_id: " + str(results[0]) + "     Activity: "+str(results[1]))
         # plt.savefig(path_store + 'user_' + str(results[0])+ '_activity_'+str(results[1])+'.png', bbox_inches='tight')
-    fig.subplots_adjust(top=0.89, left=0.1, right=0.9, bottom=0.12, hspace = 0.25)
+    fig.subplots_adjust(top=0.89, left=0.06, right=0.98, bottom=0.12, hspace = 0.25)
     #axs.flatten()[-1].legend(loc='lower center', bbox_to_anchor=(0.5, -0.5), ncol=2)
     plt.suptitle("User_in_role_id: " + str(user) + "     Activity: "+ activity)
     plt.rcParams["figure.figsize"]=[20.0, 10.0]
