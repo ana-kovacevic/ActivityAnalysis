7a7ec72ff82e6f6bca68678a3d3dc34af3aa65a9
ana-kovacevic <ananp.90@gmail.com>
2017-12-19 01:17:26 +0100

    new plot and small changes

--- help_files.py
+++ help_files.py
@@ -7,13 +7,15 @@ dict={'AIC':234234, 'BIC':[23, 44, 55]}
 
 dict2={'aic':{'v1':12, 'v2':123}}
 
-a={'Activity':'sadasd', 'BIC':12312, 'AIC':23234}
+a={'Activity':'sadasd', 'BIC':12.312, 'AIC':23234}
 b={'Activity':'aa', 'BIC':12, 'AIC':234}
+li = []
+li.append(a)
+li.append(b)
 
-c=pd.DataFrame([a,b])
+c=pd.DataFrame(li)
+c
+c.to_csv('ana.csv')
 
-
-
-
-g=5
+type(c)
 
